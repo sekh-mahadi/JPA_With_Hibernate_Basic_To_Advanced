ec939f7f37afa7731c118f75915c20ba2ae4309d
sekh-mahadi <sekh.mahadi2007cse@gmail.com>
2021-04-26 10:24:47 +0600

    Completed JPA Hierarchy.

--- Jpa-Advanced/src/main/java/io/mtech/jpa/hibernate/JpaAdvancedApplication.java
+++ Jpa-Advanced/src/main/java/io/mtech/jpa/hibernate/JpaAdvancedApplication.java
@@ -1,5 +1,7 @@
 package io.mtech.jpa.hibernate;
 
+import java.math.BigDecimal;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -7,8 +9,12 @@ import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import io.mtech.jpa.hibernate.entity.Course;
+import io.mtech.jpa.hibernate.entity.Employee;
+import io.mtech.jpa.hibernate.entity.FullTimeEmployee;
+import io.mtech.jpa.hibernate.entity.PartTimeEmployee;
 import io.mtech.jpa.hibernate.entity.Student;
 import io.mtech.jpa.hibernate.repo.CourseRepository;
+import io.mtech.jpa.hibernate.repo.EmployeeRepository;
 import io.mtech.jpa.hibernate.repo.StudentRepository;
 import lombok.extern.slf4j.Slf4j;
 
@@ -26,13 +32,15 @@ public class JpaAdvancedApplication implements CommandLineRunner {
 	@Autowired
 	StudentRepository studentRepository;
 
+	@Autowired
+	EmployeeRepository employeeRepository;
+
 	@Override
 	public void run(String... args) throws Exception {
 
-
-		//courseRepository.playWithEntityManager();
-		//studentRepository.saveStudentWithPassport();
-		//courseRepository.addHardCodedReviewsForCourse();
+		// courseRepository.playWithEntityManager();
+		// studentRepository.saveStudentWithPassport();
+		// courseRepository.addHardCodedReviewsForCourse();
 
 //		List<Review> reviews = new ArrayList<>();
 //		reviews.add(new Review("5", "Great Hands-on Stuff."));
@@ -42,9 +50,17 @@ public class JpaAdvancedApplication implements CommandLineRunner {
 		 * repository.deleteById(100001l); repository.save(new
 		 * Course(10004l,"Microservices basic to advanced"));
 		 */
-		//studentRepository.insertHardCodedStudentAndCourse();
-		studentRepository.insertCourseAndStudent( new Course("Kubernates and Docker-updated.."), new Student("Tamanna"));
-	
+		// studentRepository.insertHardCodedStudentAndCourse();
+		// studentRepository.insertCourseAndStudent( new Course("Kubernates and
+		// Docker-updated.."), new Student("Tamanna"));
+		
+		//Sekh-Full time Employee wage-1000$
+		//Rupon-Part time Employee wage-100$
+		employeeRepository.insert(new PartTimeEmployee("Shekh Rupon", new BigDecimal("100")));
+		employeeRepository.insert(new FullTimeEmployee("Sekh Mahadi", new BigDecimal("1000")));
+		log.info("All Full time Employess:->{} ", employeeRepository.allPartTimeEmployees());
+		log.info("All Part time Employess:->{} ", employeeRepository.allFullTimeEmployees());
+
 	}
 
 }
